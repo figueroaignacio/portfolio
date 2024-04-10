05e8699191faa755c73bc1b3fe5b5be4a626e4a5
figueroaignacio <figueroaignaciodev@gmail.com>
2024-04-10 19:49:35 -0300

    style: changing the color of some icons

--- src/icons/Mail.tsx
+++ src/icons/Mail.tsx
@@ -6,7 +6,7 @@ export function Mail() {
       height="16"
       viewBox="0 0 24 24"
       fill="none"
-      stroke="#ea4335"
+      stroke="#f5f5f5"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
