cb98be59f241625f058be0c2df1c03f8f1e3b690
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-17 15:59:12 -0300

    chore: changing some og image styles

--- src/components/OgImage.tsx
+++ src/components/OgImage.tsx
@@ -1,13 +1,13 @@
 export default function OgImage() {
   return (
     <div>
-      <h1 style={{ fontSize: "4rem", fontWeight: "bold", opacity: ".9" }}>
+      <h1 style={{ fontSize: "3rem", fontWeight: "bold", opacity: ".9" }}>
         Hey, I'm Ignacio Figueroa
       </h1>
-      <p style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+      <p style={{ fontSize: "1rem", fontWeight: "bold" }}>
         Software Developer with a strong focus in the Frontend side.
       </p>
-      <p>Let's work together!</p>
+      <p>Let's work together! 🚀</p>
     </div>
   );
 }
