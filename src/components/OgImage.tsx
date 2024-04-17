d2376bdfe51e2d5d44835d2ef615809fa8b050a6
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-04-17 15:42:28 -0300

    feat: adding meta tags and og image

---
+++ src/components/OgImage.tsx
@@ -0,0 +1,13 @@
+export default function OgImage() {
+  return (
+    <div>
+      <h1 style={{ fontSize: "4rem", fontWeight: "bold", opacity: ".9" }}>
+        Hey, I'm Ignacio Figueroa
+      </h1>
+      <p style={{ fontSize: "1.5rem", fontWeight: "bold" }}>
+        Software Developer with a strong focus in the Frontend side.
+      </p>
+      <p>Let's work together!</p>
+    </div>
+  );
+}
