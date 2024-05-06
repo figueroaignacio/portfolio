a03b71d3bec447fb68438cedae92441eb7d3ad9f
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-06 15:05:12 -0300

    feat(constants): adding constants folder and contact object

---
+++ src/constants/contact.ts
@@ -0,0 +1,14 @@
+export const CONTACTS = [
+  {
+    title: "Mail",
+    href: "mailto:ignaciofigueroadev@gmail.com",
+  },
+  {
+    title: "LinkedIn",
+    href: "https://www.linkedin.com/in/ignacio-figueroa-0a1ba0263/",
+  },
+  {
+    title: "GitHub",
+    href: "https://github.com/figueroaignacio/",
+  },
+];
