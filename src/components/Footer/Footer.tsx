b539c93b9ccf3c8f45050dae5f3f9864667d7f65
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-05-06 15:09:28 -0300

    refactor(components): refactoring footer code

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,11 +4,11 @@ import { useTranslation } from "react-i18next";
 // Styles
 import styles from "./Footer.module.css";
 
+// Constants
+import { CONTACTS } from "../../constants/contact";
+
 // Icons
 import { Chevron } from "../../icons/Chevron";
-import { Github } from "../../icons/Github";
-import { LinkedIn } from "../../icons/LinkedIn";
-import { Mail } from "../../icons/Mail";
 
 export function Footer() {
   const { t } = useTranslation();
@@ -23,30 +23,12 @@ export function Footer() {
       <div>
         <h2 className={styles.footerTitle}>{t("footer.contactTitle")}</h2>
         <ul className={styles.footerList}>
-          <li className={styles.footerListItem}>
-            <a href="mailto:ignaciofigueroadev@gmail.com" target="_blank">
-              <Mail />
-              Mail
-            </a>
-            <Chevron />
-          </li>
-          <li className={styles.footerListItem}>
-            <a
-              href="https://www.linkedin.com/in/ignacio-figueroa-0a1ba0263/"
-              target="_blank"
-            >
-              <LinkedIn />
-              LinkedIn
-            </a>
-            <Chevron />
-          </li>
-          <li className={styles.footerListItem}>
-            <a href="https://github.com/figueroaignacio" target="_blank">
-              <Github />
-              Github
-            </a>
-            <Chevron />
-          </li>
+          {CONTACTS.map((item, index: number) => (
+            <li key={index} className={styles.footerListItem}>
+              <a href={item.href}>{item.title}</a>
+              <Chevron />
+            </li>
+          ))}
         </ul>
       </div>
       <div>
