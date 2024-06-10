80609d3b1447c6ba378fdfbe7beb164d47359462
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-06-10 17:19:55 -0300

    style(components): changing the separator styles

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -5,8 +5,6 @@ export function Separator() {
   return (
     <div className={styles.container}>
       <div className={styles.border} />
-      <span className={styles.content}>O</span>
-      <div className={styles.border} />
     </div>
   );
 }
