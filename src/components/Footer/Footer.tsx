dc0901abe3f7b1d5168b048e2c2b38745c68e143
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-06-14 03:00:43 -0300

    style: changing footer styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,11 @@
+// Components
+import { Contacts } from "../Contacts/Contacts";
+
 // i18n
 import { useTranslation } from "react-i18next";
 
 // Styles
 import styles from "./Footer.module.css";
-
-// Constants
-import { CONTACTS } from "../../constants/contact";
-
-// Icons
-import { Chevron } from "../../icons/Chevron";
 
 export function Footer() {
   const { t } = useTranslation();
@@ -18,20 +15,10 @@ export function Footer() {
       <div>
         <h2>{t("footer.title")}</h2>
         <p>{t("footer.subtitle")}</p>
-        <p>{t("footer.paragraph")}</p>
       </div>
       <div>
         <h2 className={styles.footerTitle}>{t("footer.contactTitle")}</h2>
-        <ul className={styles.footerList}>
-          {CONTACTS.map((item, index: number) => (
-            <li key={index} className={styles.footerListItem}>
-              <a href={item.href} target="_blank">
-                {item.title}
-              </a>
-              <Chevron />
-            </li>
-          ))}
-        </ul>
+        <Contacts />
       </div>
       <div>
         <p>{t("footer.builtIn")}</p>
