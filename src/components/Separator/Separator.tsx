7b6ca041dbaace419dc6ce5c196f4745b2658307
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-13 00:46:57 -0300

    feat: add fade up to the separator

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -1,10 +1,13 @@
 // Styles
+import { FadeUp } from "../FramerAnimations/FadeUp";
 import styles from "./Separator.module.css";
 
 export function Separator() {
   return (
-    <div className={styles.container}>
-      <div className={styles.border} />
-    </div>
+    <FadeUp delay={0.5}>
+      <div className={styles.container}>
+        <div className={styles.border} />
+      </div>
+    </FadeUp>
   );
 }
