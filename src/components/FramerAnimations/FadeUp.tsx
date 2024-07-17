8abd44adaa8a00b5582ff06d462ce53201fc613d
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-16 23:24:25 -0300

    chore: change animation duration

--- src/components/FramerAnimations/FadeUp.tsx
+++ src/components/FramerAnimations/FadeUp.tsx
@@ -12,7 +12,7 @@ export const FadeUp = ({ children, delay }: FadeUpProps) => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{
-        duration: 0.5,
+        duration: 0.4,
         delay: delay,
         ease: "easeOut",
       }}
