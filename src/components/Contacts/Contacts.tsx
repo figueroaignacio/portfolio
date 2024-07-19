e3929bafb9057e9c1614d853bd6f4665468a5681
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-19 00:51:06 -0300

    feat: add contacts traductions

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,15 +1,24 @@
+// i18n
+import { useTranslation } from "react-i18next";
+
+// Icons
 import { Github } from "../../icons/Github";
 import { LinkedIn } from "../../icons/LinkedIn";
 import { Mail } from "../../icons/Mail";
 
+// Styles
 import styles from "./Contacts.module.css";
 
+// Constants
 import { CONTACTS } from "../../constants/contact";
 
 export function Contacts() {
+  const { t } = useTranslation();
+
   return (
     <section className={styles.contactsSection}>
-      <h2 className={styles.contactsTitle}>Contacto</h2>
+      <h2 className={styles.contactsTitle}>{t("contact.title")}</h2>
+      <p>{t("contact.description")}</p>
       <div className={styles.contactsBox}>
         {CONTACTS.map((contact, index) => {
           let iconComponent;
