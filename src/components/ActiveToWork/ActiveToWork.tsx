cc8de180559832a054029325545624211dd24511
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-07-24 00:12:12 -0300

    feat: add active to work translations

--- src/components/ActiveToWork/ActiveToWork.tsx
+++ src/components/ActiveToWork/ActiveToWork.tsx
@@ -1,9 +1,16 @@
+// i18n
+import { useTranslation } from "react-i18next";
+
 // Styles
 import styles from "./ActiveToWork.module.css";
 
-export const ActiveToWork = () => (
-  <div className={styles.container}>
-    <div className={styles.pulsatingDot} />
-    <span>Active to work</span>
-  </div>
-);
+export function ActiveToWork() {
+  const { t } = useTranslation();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.pulsatingDot} />
+      <span>{t("activeToWork.title")}</span>
+    </div>
+  );
+}
