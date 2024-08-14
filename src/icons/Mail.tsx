b0523d6fbddfdb5a48e1de5d96d01d53409624ae
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-08-14 14:12:52 -0300

    fix: fix attributes

--- src/icons/Mail.tsx
+++ src/icons/Mail.tsx
@@ -12,9 +12,9 @@ export function Mail({ height, width }: IconProps) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="#f5f5f5"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <title>Mail Icon</title>
       <rect width="20" height="16" x="2" y="4" rx="2"></rect>
