8e3104f56e62c6b5814573e154ea0531780b799f
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-09-13 17:26:07 -0300

    feat: add view cv button

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -20,14 +20,23 @@ export function About() {
     >
       <h2 className={styles.aboutTitle}>{t("aboutMe.title")}</h2>
       <p>{t("aboutMe.description")}</p>
-      <FramerDiv variants={FADE_DOWN_ANIMATION_VARIANTS}>
+      <FramerDiv
+        variants={FADE_DOWN_ANIMATION_VARIANTS}
+        className={styles.aboutLinks}
+      >
+        <a
+          href={t("aboutMe.viewCV.path")}
+          className={styles.aboutButton}
+          target="_blank"
+        >
+          {t("aboutMe.viewCV.title")}
+        </a>
         <a
           href={t("aboutMe.downloadCV.path")}
           download={t("aboutMe.downloadCV.downloadName")}
+          className={styles.aboutButton}
         >
-          <button className={styles.aboutButton}>
-            {t("aboutMe.downloadCV.title")}
-          </button>
+          {t("aboutMe.downloadCV.title")}
         </a>
       </FramerDiv>
     </FramerSection>
