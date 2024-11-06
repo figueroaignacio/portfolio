427b3f72164dea366f0059f6fad4f5314a078ca9
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-06 00:20:35 -0300

    chore: formatting all the code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,25 +46,28 @@ export function Navbar() {
         <div className="flex items-center gap-x-3">
           <ToggleTheme />
           <SelectLanguage />
-          <div className="md:hidden"
-               onClick={toggleMenu}>
+          <div
+            className="md:hidden"
+            onClick={toggleMenu}>
             <Menu />
           </div>
         </div>
         <nav className={`absolute top-0 left-0 transition-transform bg-background md:bg-transparent min-h-dvh md:min-h-0 duration-300 transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:static md:hidden z-30`}>
           <div className="flex justify-end p-2">
-            <button onClick={toggleMenu}
-                    aria-label="Close menu"
-                    className="md:hidden">
+            <button
+              onClick={toggleMenu}
+              aria-label="Close menu"
+              className="md:hidden">
               <X />
             </button>
           </div>
           <ul className="flex flex-col space-y-5 rounded-md">
             {navItems.map((navItem, index) => (
               <li key={index}>
-                <Link to={navItem.href}
-                      onClick={toggleMenu}
-                      className="text-muted-foreground hover:text-muted font-semibold duration-100">
+                <Link
+                  to={navItem.href}
+                  onClick={toggleMenu}
+                  className="text-muted-foreground hover:text-muted font-semibold duration-100">
                   {navItem.label}
                 </Link>
               </li>
@@ -79,8 +82,9 @@ export function Navbar() {
         </nav>
       </header>
       {isOpen && (
-        <div className="fixed inset-0 backdrop-blur-sm z-10"
-             onClick={toggleMenu} />
+        <div
+          className="fixed inset-0 backdrop-blur-sm z-10"
+          onClick={toggleMenu} />
       )}
     </>
 
