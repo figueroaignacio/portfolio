69b47cfc8bb25725819ecce0882412c2f0421eb1
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-05 21:43:40 -0300

    feat: change favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,6 +4,6 @@ import { Link } from "react-router-dom";
 export function Logo() {
   return (
     <Link to="/"
-          className="bg-primary-foreground py-3 px-4 rounded-md font-semibold">N.</Link>
+          className="bg-primary-foreground py-2 px-3 rounded-md font-bold text-xl">N.</Link>
   )
 }
