46b499bd93df2f65e6ce0e0bf8a4962579e1d461
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-06 20:04:19 -0300

    feat: add experience section

---
+++ src/components/ExperienceCard.tsx
@@ -0,0 +1,53 @@
+// Hooks
+import { useTranslation } from "react-i18next";
+
+interface ExperienceCardProps {
+  role: string;
+  description: string;
+  company: string;
+  datetime: string;
+  responsibilities: string[];
+  techStack: string[];
+}
+
+export function ExperienceCard({ company, datetime, responsibilities, role, description, techStack }: ExperienceCardProps) {
+  const { t } = useTranslation()
+
+  return (
+    <div className="bg-card rounded-lg p-8 my-6 transition-all duration-300 border-[1px] border-border">
+      <div className="flex flex-col md:flex-row md:items-start md:justify-between mb-6">
+        <div className="flex-grow">
+          <h3 className="text-2xl font-bold mb-1 gradient-text">{role}</h3>
+          <h4 className="text-base font-medium text-muted-foreground">{company}</h4>
+        </div>
+        <span className="text-sm mt-2 md:mt-0 text-muted-foreground whitespace-nowrap">{datetime}</span>
+      </div>
+      <p className="mb-6 text-sm leading-relaxed">{description}</p>
+      <div className="mb-6">
+        <h5 className="font-semibold text-sm mb-3">{t("components.experienceCard.responsibilities.label")}</h5>
+        <ul className="space-y-2">
+          {responsibilities.map((responsibility, index) => (
+            <li
+              key={index}
+              className="flex items-start text-sm text-muted-foreground">
+              <span className="mr-2">•</span>
+              <span>{responsibility}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h5 className="font-semibold text-sm mb-3">{t("components.experienceCard.techStack.label")}</h5>
+        <ul className="flex flex-wrap gap-2">
+          {techStack.map((tech, index) => (
+            <li
+              key={index}
+              className="text-xs bg-primary-foreground py-1 px-3 rounded-full">
+              {tech}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,10 @@ import React from "react"
 
 // Hooks
 import { Trans, useTranslation } from "react-i18next"
+
+// Components
 import { Link } from "react-router-dom"
+import { ExperienceCard } from "@/components/ExperienceCard.tsx";
 
 // Icons
 import { Chevron, User } from "@/components/Icons.tsx"
@@ -11,8 +14,18 @@ import { Chevron, User } from "@/components/Icons.tsx"
 import { motion } from "framer-motion"
 import { containerVariants, fadeUpVariants } from "@/constants/animations.ts";
 
+type Experience = {
+  company: string;
+  datetime: string;
+  role: string;
+  responsibilities: string[];
+  description: string;
+  techStack: string[];
+}
+
 export function Home() {
   const { t } = useTranslation()
+  const experience = t("sections.experience.items", { returnObjects: true }) as Experience[]
 
   const links: { href: string; label: string; icon: React.ReactNode }[] = [
     {
@@ -28,42 +41,66 @@ export function Home() {
   ]
 
   return (
-    <motion.section
-      className="min-h-[80dvh] flex flex-col justify-center gap-y-3"
+    <motion.div
       variants={containerVariants}
       initial="hidden"
-      animate="visible"
-    >
-      <motion.h1
-        className="text-5xl font-bold py-2"
-        variants={fadeUpVariants}>
-        {t("sections.home.title")}
-      </motion.h1>
-      <motion.h2
-        className="text-muted-foreground"
-        variants={fadeUpVariants}>
-        <Trans
-          i18nKey="sections.home.subtitle"
-          components={{ span: <span className="gradient-text" /> }}
-        />
-      </motion.h2>
-      <div
-        className="flex gap-x-2"
+      animate="visible">
+      <section
+        className="min-h-[80dvh] flex flex-col justify-center gap-y-3"
       >
-        {links.map((link, index) => (
-          <motion.div
-            key={index}
-            variants={fadeUpVariants}>
-            <Link
-              to={link.href}
-              className="flex items-center gap-2 px-6 py-2 rounded-md text-sm bg-primary hover:bg-primary-foreground duration-100 border-[1px] border-border"
-            >
-              {link.label}
-              {link.icon}
-            </Link>
-          </motion.div>
-        ))}
-      </div>
-    </motion.section>
+        <motion.h1
+          className="text-5xl font-bold py-2"
+          variants={fadeUpVariants}>
+          {t("sections.home.title")}
+        </motion.h1>
+        <motion.h2
+          className="text-muted-foreground"
+          variants={fadeUpVariants}>
+          <Trans
+            i18nKey="sections.home.subtitle"
+            components={{ span: <span className="gradient-text" /> }}
+          />
+        </motion.h2>
+        <div
+          className="flex gap-x-2"
+        >
+          {links.map((link, index) => (
+            <motion.div
+              key={index}
+              variants={fadeUpVariants}>
+              <Link
+                to={link.href}
+                className="flex items-center gap-2 px-6 py-2 rounded-md text-sm bg-primary hover:bg-primary-foreground duration-100 border-[1px] border-border"
+              >
+                {link.label}
+                {link.icon}
+              </Link>
+            </motion.div>
+          ))}
+        </div>
+      </section>
+      <section>
+        <motion.h2
+          className="font-bold text-xl"
+          variants={fadeUpVariants}>{t("sections.experience.title")}
+        </motion.h2>
+        <ul>
+          {experience.map((item, index) => (
+            <motion.li
+              key={index}
+              variants={fadeUpVariants}>
+              <ExperienceCard
+                responsibilities={item.responsibilities}
+                datetime={item.datetime}
+                company={item.company}
+                role={item.role}
+                description={item.description}
+                techStack={item.techStack}
+              />
+            </motion.li>
+          ))}
+        </ul>
+      </section>
+    </motion.div>
   )
 }
