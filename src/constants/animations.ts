4bf92dccedfc2262a6df61724386663093e530ef
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-05 21:53:53 -0300

    feat: add animations to hero

---
+++ src/constants/animations.ts
@@ -0,0 +1,34 @@
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+const fadeUpVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 100,
+      damping: 10,
+    },
+  },
+}
+
+const fadeInVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+}
+
+export { containerVariants, fadeInVariants, fadeUpVariants }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React from "react"
 
 // Hooks
-import { useTranslation } from "react-i18next";
-
-// Components
-import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next"
+import { Link } from "react-router-dom"
 
 // Icons
-import { Chevron, User } from "@/components/Icons.tsx";
+import { Chevron, User } from "@/components/Icons.tsx"
+
+// Utils
+import { motion } from "framer-motion"
+import { containerVariants, fadeUpVariants } from "@/constants/animations.ts";
 
 export function Home() {
   const { t } = useTranslation()
 
-  const links: { href: string, label: string, icon: React.ReactNode }[] = [
+  const links: { href: string; label: string; icon: React.ReactNode }[] = [
     {
       href: "/projects",
       label: "Projects",
@@ -22,26 +24,40 @@ export function Home() {
       href: "/about",
       label: "About me",
       icon: <User />,
-    }
+    },
   ]
 
   return (
-    <section className="min-h-[85dvh] flex flex-col justify-center gap-y-3">
-      <h1 className="text-4xl font-bold">
+    <motion.section
+      className="min-h-[85dvh] flex flex-col justify-center gap-y-3"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.h1 className="text-4xl font-bold"
+                 variants={fadeUpVariants}>
         {t("sections.home.title")}
-      </h1>
-      <h2 className="text-muted-foreground">{t("sections.home.subtitle")}</h2>
-      <div className="flex gap-x-2">
+      </motion.h1>
+      <motion.h2 className="text-muted-foreground"
+                 variants={fadeUpVariants}>
+        {t("sections.home.subtitle")}
+      </motion.h2>
+      <motion.div className="flex gap-x-2"
+                  variants={fadeUpVariants}>
         {links.map((link, index) => (
-          <Link
+          <div
             key={index}
-            to={link.href}
-            className="flex items-center gap-2 px-6 py-2 rounded-md text-sm bg-primary hover:bg-primary-foreground duration-100 border-[1px] border-border">
-            {link.label}
-            {link.icon}
-          </Link>
+          >
+            <Link
+              to={link.href}
+              className="flex items-center gap-2 px-6 py-2 rounded-md text-sm bg-primary hover:bg-primary-foreground duration-100 border-[1px] border-border"
+            >
+              {link.label}
+              {link.icon}
+            </Link>
+          </div>
         ))}
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   )
 }
