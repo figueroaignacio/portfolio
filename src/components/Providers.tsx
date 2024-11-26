51790eacca71186a7d294a94dcc81c0774be5c0c
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-25 22:27:49 -0300

    chore: change interface name

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,11 +2,11 @@ import { LanguageProvider } from "@/contexts/LanguageContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import React from "react";
 
-interface AppProvidersProps {
+interface ProvidersProps {
   children: React.ReactNode;
 }
 
-export const Providers: React.FC<AppProvidersProps> = ({ children }) => {
+export const Providers: React.FC<ProvidersProps> = ({ children }) => {
   return (
     <ThemeProvider>
       <LanguageProvider>{children}</LanguageProvider>
