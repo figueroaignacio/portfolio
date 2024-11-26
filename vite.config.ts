7ce6a2c32d2981ea56307757c4bbf8f4ae03e15d
figueroaignacio <ignaciofigueroadev@gmail.com>
2024-11-26 16:37:41 -0300

    chore: add config files

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    open: true,
+  },
 });
