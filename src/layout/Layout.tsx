18de1a399ee9323ca4e558d346d002c6283a511e
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-09 00:34:14 -0300

    fix: mobile view

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 
 export function Layout() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-12 lg:border lg:border-border mx-5 my-3 rounded-md">
+    <div className="grid grid-cols-1 lg:grid-cols-12 lg:border lg:border-border lg:mx-5 my-3 rounded-md">
       <div className="col-span-5">
         <ProfileInfo />
       </div>
