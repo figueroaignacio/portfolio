96acf98cf6e6a438feaa37d0ea78ff97ad2f0a45
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-09 17:38:53 -0300

    style: layout height

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,13 +5,13 @@ import { Outlet } from "react-router-dom";
 
 export function Layout() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-12 lg:border lg:border-border lg:mx-5 my-3 rounded-md">
-      <div className="col-span-5">
+    <div className="grid grid-cols-1 lg:grid-cols-12 lg:border lg:border-border rounded-md min-h-dvh">
+      <div className="col-span-5 flex flex-col">
         <ProfileInfo />
       </div>
-      <main className="col-span-7 lg:p-6">
+      <main className="col-span-7 lg:p-6 flex flex-col">
         <Navbar />
-        <div className="lg:max-h-[80dvh] lg:overflow-y-scroll px-3">
+        <div className="lg:max-h-[80dvh] lg:overflow-y-scroll px-3 flex-1">
           <Outlet />
         </div>
       </main>
