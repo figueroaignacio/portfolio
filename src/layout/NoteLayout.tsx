078efc131f2e1977e1e202902b982b07c15e047e
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-01-10 09:43:25 -0300

    fix: padding

--- src/layout/NoteLayout.tsx
+++ src/layout/NoteLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export function NoteLayout() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-12 lg:p-16 min-h-screen lg:p-6">
+    <div className="grid grid-cols-1 lg:grid-cols-12 lg:p-16 min-h-screen p-6">
       <div className="lg:col-span-3 sticky top-4 self-start">
         <BackButton />
       </div>
