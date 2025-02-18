656c7040d741560314047e1816528965350f32c4
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-02-18 17:02:47 -0300

    feat: add seo component to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
+// Hooks
+import { useTranslation } from "react-i18next";
+
+// Components
+import { Seo } from "@/components/Seo";
+
 export function Home() {
-  return <section className="relative mt-6 space-y-4">Home</section>;
+  const { t } = useTranslation();
+
+  return (
+    <section className="relative mt-6 space-y-4">
+      <Seo
+        title={t("siteConfig.home.title")}
+        description={t("siteConfig.home.description")}
+      />
+    </section>
+  );
 }
