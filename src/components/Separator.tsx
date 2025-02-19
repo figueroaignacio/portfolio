a0c42d66fe2654c87d7ba8a8fb3430654dbdbe38
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-02-19 19:17:43 -0300

    style: change some borders

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -1,9 +1,9 @@
 export function Separator({ className }: { className?: string }) {
   return (
     <div className={`flex items-center gap-6 ${className}`}>
-      <div className="w-full h-[1px] bg-border"></div>
+      <div className="w-full h-[.1px] bg-border"></div>
       <div className="text-muted-foreground">o</div>
-      <div className="w-full h-[1px] bg-border"></div>
+      <div className="w-full h-[.1px] bg-border"></div>
     </div>
   );
 }
