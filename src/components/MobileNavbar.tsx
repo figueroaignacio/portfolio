7ec2577ef93b284acbf382ee98a7c36b28b5af7c
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-02-27 17:54:19 -0300

    fix: navbar responsive

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -8,7 +8,7 @@ export const MobileNavbar = () => {
   const navItems = useNavItems();
 
   return (
-    <header className="fixed md:sticky md:top-4 bottom-5 left-0 right-0 md:bottom-auto z-20 m-2 rounded-md backdrop-blur-2xl bg-slate-200/75 dark:bg-[#111317]/20 lg:hidden">
+    <header className="fixed md:sticky md:top-4 bottom-5 left-0 right-0 md:bottom-auto z-20 m-2 rounded-md backdrop-blur-2xl bg-slate-200/75 dark:bg-[#111317]/20 md:hidden">
       <nav className="max-w-screen-xl mx-auto px-4">
         <NavLinks
           navItems={navItems}
