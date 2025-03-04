97d59051e5028e38cbe4c1fc8184c639b054945d
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-03-04 02:09:47 -0300

    feat: add scroll to top component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // Components
 import { AppRouter } from "@/components/Router.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <AppRouter />
     </Router>
   );
