a58ba9c493496d032316ef7eada2429cb46db606
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-03-12 15:25:51 -0300

    fix: fix the flash of unstyled content

--- src/hooks/contexts/ThemeContext.tsx
+++ src/hooks/contexts/ThemeContext.tsx
@@ -1,7 +1,11 @@
-// React
-import React, { createContext, useEffect, useState } from "react";
+import React, {
+  createContext,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from "react";
 
-type Theme = typeof LIGHT_THEME | typeof DARK_THEME;
+type Theme = "light" | "dark";
 
 interface ThemeContextProps {
   theme: Theme;
@@ -12,40 +16,57 @@ interface ThemeContextProps {
 const LIGHT_THEME = "light";
 const DARK_THEME = "dark";
 const MEDIA = "(prefers-color-scheme: dark)";
+
 export const ThemeContext = createContext<ThemeContextProps | undefined>(
   undefined
 );
 
+const getInitialTheme = (): Theme => {
+  if (typeof window !== "undefined") {
+    const storedTheme = localStorage.getItem("theme") as Theme | null;
+    if (storedTheme === LIGHT_THEME || storedTheme === DARK_THEME) {
+      return storedTheme;
+    }
+    if (window.matchMedia && window.matchMedia(MEDIA).matches) {
+      return DARK_THEME;
+    }
+  }
+
+  return LIGHT_THEME;
+};
+
+const UseIsomorphicLayoutEffect =
+  typeof window !== "undefined" ? useLayoutEffect : useEffect;
+
+const applyTheme = (theme: Theme) => {
+  if (typeof document !== "undefined") {
+    const root = document.documentElement;
+    root.classList.remove(LIGHT_THEME, DARK_THEME);
+    root.classList.add(theme);
+  }
+};
+
+if (typeof window !== "undefined") {
+  const initialTheme = getInitialTheme();
+  applyTheme(initialTheme);
+}
+
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [theme, setTheme] = useState<Theme | null>(null);
+  const [theme, setTheme] = useState<Theme>(getInitialTheme());
 
-  useEffect(() => {
-    const storedTheme = localStorage.getItem("theme") as Theme | null;
-    const prefersDark = window.matchMedia(MEDIA).matches;
-    setTheme(storedTheme || (prefersDark ? DARK_THEME : LIGHT_THEME));
-  }, []);
-
-  useEffect(() => {
-    if (theme) {
-      const root = window.document.documentElement;
-      root.classList.remove(LIGHT_THEME, DARK_THEME);
-      root.classList.add(theme);
-      localStorage.setItem("theme", theme);
-    }
+  UseIsomorphicLayoutEffect(() => {
+    applyTheme(theme);
+    localStorage.setItem("theme", theme);
   }, [theme]);
 
   const toggleTheme = () => {
-    if (theme) {
-      setTheme(theme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME);
-    }
+    setTheme(theme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME);
   };
 
   return (
-    <ThemeContext.Provider
-      value={{ theme: theme || LIGHT_THEME, setTheme, toggleTheme }}
-    >
+    <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
