9191199135b1ac372ccb91cd99fa6154dd06ffbc
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-03-13 19:25:01 -0300

    feat: improve alert component

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,10 +1,12 @@
-import { InfoIcon } from "./Icons";
+import { TerminalIcon } from "./Icons";
 
 export function Alert({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col gap-3 p-4 mb-4 bg-[#FFF9C4] dark:bg-[#FFF176]/60 rounded-2xl">
-      <InfoIcon />
-      <span className="text-xs">{children}</span>
+    <div className="flex justify-center gap-3 p-4 mb-4 border-border border rounded-2xl ">
+      <div className="flex-1">
+        <TerminalIcon />
+      </div>
+      <span className="text-xs leading-5">{children}</span>
     </div>
   );
 }
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -471,6 +471,25 @@ function InfoIcon() {
   );
 }
 
+function TerminalIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <polyline points="4 17 10 11 4 5" />
+      <line x1="12" x2="20" y1="19" y2="19" />
+    </svg>
+  );
+}
+
 export {
   AboutIcon,
   ArrowLeftIcon,
@@ -496,6 +515,7 @@ export {
   ProjectsIcon,
   ScrollToTopIcon,
   SunIcon,
+  TerminalIcon,
   User,
   X,
 };
