c41c4dafa64e6aad298a089a3d836584fc799b9f
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-14 16:12:16 -0300

    feat: remove project

--- content/README.md
+++
@@ -1,1 +0,0 @@
-# Yes I'm using Sanity
--- content/schemaTypes/index.ts
+++
@@ -1,6 +0,0 @@
-import blockContent from './blockContent'
-import category from './category'
-import note from './note'
-import project from './project'
-
-export const schemaTypes = [note, category, blockContent, project]
