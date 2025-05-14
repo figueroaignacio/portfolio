99894d0b9d43605e636618baff97adde4f7ec874
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-14 18:10:29 -0300

    feat: add root layout grid

--- src/layout/root-layout.tsx
+++ src/layout/root-layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router';
 
 export function RootLayout() {
   return (
-    <div>
+    <div className="root-layout-grid">
       <Header />
       <main>
         <Outlet />
