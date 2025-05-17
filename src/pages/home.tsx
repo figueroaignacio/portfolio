c6280127ab62a0cf94273030895506712bd40a7f
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-16 22:37:03 -0300

    chore: remove hero and about section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,3 @@
-// Sections
-import { About } from '@/sections/about';
-import { Hero } from '@/sections/hero';
-
 export function HomePage() {
-  return (
-    <div>
-      <Hero />
-      <About />
-    </div>
-  );
+  return <div>Hello</div>;
 }
