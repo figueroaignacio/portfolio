0f8f609a203584dc71cd49ef452b3657d1b272f7
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-17 17:34:05 -0300

    fix: css import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 // CSS
-import './index.css';
+import '@/styles/index.css';
 
 // Components
 import App from './App.tsx';
