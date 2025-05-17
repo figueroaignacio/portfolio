d83394a600533c4b8044e86afac30161702a9d93
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-17 11:26:13 -0300

    refactor: add header links component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,11 +5,9 @@ import { useLocation } from 'react-router';
 // Components
 import { HomeIcon } from '@radix-ui/react-icons';
 import { Link } from 'react-router';
+import { HeaderLinks } from './header-links';
 import { LanguageSwitcher } from './language-switcher';
 import { ToggleTheme } from './toggle-theme';
-
-// Constants
-import { navigation } from '@/lib/navigation';
 
 export function Header() {
   const location = useLocation();
@@ -51,26 +49,12 @@ export function Header() {
       <div className="container flex items-center justify-between">
         <Link
           to="/"
-          className={`btn-icon ${currentPath === '/' ? 'text-foreground border-border' : 'text-muted-foreground'}`}
+          className={`btn-icon hover:text-foreground transition-colors duration-100 ${currentPath === '/' ? 'text-foreground border-border' : 'text-muted-foreground'}`}
         >
           <HomeIcon className="size-5" />
         </Link>
         <nav>
-          <ul className="flex gap-x-7">
-            {navigation.map((item, index) => {
-              const isActive = currentPath === item.href;
-              return (
-                <li key={index}>
-                  <Link
-                    to={item.href}
-                    className={`btn-icon ${isActive ? 'border-border text-foreground' : 'text-muted-foreground'}`}
-                  >
-                    <item.icon className="hover:text-foreground size-5" />
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
+          <HeaderLinks />
         </nav>
         <div className="flex gap-x-5">
           <ToggleTheme />
