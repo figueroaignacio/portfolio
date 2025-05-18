a4d0e23837b47cf546e51385fb31de11713dec50
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-18 00:18:03 -0300

    feat: add scroll to top behaviour

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+// Components
 import { AppRouter } from './components/app-router';
+import { ScrollToTop } from './components/scroll-to-top';
 
 export default function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <ScrollToTop />
+      <AppRouter />
+    </>
+  );
 }
---
+++ src/components/scroll-to-top.tsx
@@ -0,0 +1,13 @@
+// Hooks
+import { useEffect } from 'react';
+import { useLocation } from 'react-router';
+
+export function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, [pathname]);
+
+  return null;
+}
