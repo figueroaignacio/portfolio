8b10955a48d54d813c340f352ff4adafbf486585
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-05-21 16:27:08 -0300

    style: change education project border

--- src/components/education-card.tsx
+++ src/components/education-card.tsx
@@ -6,7 +6,7 @@ import { type Education } from '@/types';
 
 export function EducationCard({ date, description, place, tags, title }: Education) {
   return (
-    <div className="border-primary space-y-5 rounded-md border-l-8 py-4 pl-4">
+    <div className="border-primary space-y-5 rounded-md border-t-8 px-4 py-4 shadow-sm">
       <header className="space-y-3">
         <div className="flex flex-wrap-reverse justify-between gap-y-3">
           <div className="space-y-1">
