f2e87823c8a5d3e566d93332f34c865b6b3cec53
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-07-12 20:40:57 -0300

    feat: remove to start with next.js

--- src/components/badge.tsx
+++
@@ -1,8 +0,0 @@
-interface BadgeProps {
-  children: React.ReactNode;
-  variant?: 'badge' | 'badge-primary' | 'badge-secondary';
-}
-
-export function Badge({ children, variant }: BadgeProps) {
-  return <span className={`w-fit ${variant}`}>{children}</span>;
-}
