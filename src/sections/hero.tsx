495a3491b2f45c06f926dfc2357fb6866de77905
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-07-19 17:07:13 -0300

    feat: add hero section

---
+++ src/sections/hero.tsx
@@ -0,0 +1,11 @@
+export function Hero() {
+  return (
+    <section className="space-y-1">
+      <h1 className="text-2xl">Hi, I'm Ignacio</h1>
+      <p className="text-sm">
+        Software Developer based in Argentina, currently open to new opportunities. Feel free to
+        reach out. At the moment, building i7a/ui and Commit Emocional.
+      </p>
+    </section>
+  );
+}
